5abea29262fe317da0233f903011ce4db85c52b6
Shubham Gantayat <sgantayat9@gmail.com>
2021-09-17 22:30:55 +0530

    mlflow-commit

--- mybash.sh
+++ mybash.sh
@@ -1,6 +1,8 @@
 git config user.email "sgantayat9@gmail.com"
 git config user.name "Shubham Gantayat"
 
+snap install --classic heroku
+
 git add .
 git commit -m "mlflow-commit"
-git push
+heroku login -i
